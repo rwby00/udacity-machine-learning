5901e7d2d45d3464f30ec8ca47de065c49654379
rwby00 <mashiro560@gmail.com>
2024-06-16 16:33:26 +0000

    Project #3 Commit

--- project3/train_model.py
+++ project3/train_model.py
@@ -1,5 +1,4 @@
-#TODO: Import your dependencies.
-#For instance, below are some dependencies you might need if you are using Pytorch
+# Import necessary dependencies
 import numpy as np
 import torch
 import torch.nn as nn
@@ -9,74 +8,80 @@ import torchvision.models as models
 import torchvision.transforms as transforms
 
 import argparse
+import sagemaker
+from sagemaker.pytorch import PyTorch
+from sagemaker.debugger import Rule, DebuggerHookConfig, ProfilerConfig, rule_configs
+
+# Import dependencies for Debugging and Profiling
+from smdebug.pytorch import get_hook
 
 
 #TODO: Import dependencies for Debugging andd Profiling
 
-def test(model, test_loader):
-    '''
-    TODO: Complete this function that can take a model and a 
-          testing data loader and will get the test accuray/loss of the model
-          Remember to include any debugging/profiling hooks that you might need
-    '''
-    pass
+def test(model, test_loader, criterion, hook):
+    model.eval()
+    hook.set_mode(sagemaker.debugger.modes.EVAL)
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    print(f"Accuracy: {100 * correct / total}%")
 
-def train(model, train_loader, criterion, optimizer):
-    '''
-    TODO: Complete this function that can take a model and
-          data loaders for training and will get train the model
-          Remember to include any debugging/profiling hooks that you might need
-    '''
-    pass
+def train(model, train_loader, criterion, optimizer, hook):
+    model.train()
+    hook.set_mode(sagemaker.debugger.modes.TRAIN)
+    for epoch in range(epochs):
+        running_loss = 0.0
+        for inputs, labels in train_loader:
+            optimizer.zero_grad()
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+        print(f"Epoch {epoch+1}, Loss: {running_loss/len(train_loader)}")
     
 def net():
-    '''
-    TODO: Complete this function that initializes your model
-          Remember to use a pretrained model
-    '''
-    pass
+    model = models.resnet50(pretrained=True)
+    for param in model.parameters():
+        param.requires_grad = False
+    model.fc = nn.Linear(2048, 133)  # Assuming 133 classes for dog breed classification
+    return model
 
 def create_data_loaders(data, batch_size):
-    '''
-    This is an optional function that you may or may not need to implement
-    depending on whether you need to use data loaders or not
-    '''
-    pass
+    transform = transforms.Compose([transforms.Resize((224, 224)),
+                                    transforms.ToTensor()])
+    trainset = torchvision.datasets.ImageFolder(root=data+'/train', transform=transform)
+    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+    testset = torchvision.datasets.ImageFolder(root=data+'/test', transform=transform)
+    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
+    return trainloader, testloader
 
 def main(args):
-    '''
-    TODO: Initialize a model by calling the net function
-    '''
-    model=net()
+    model = net()
+    hook = get_hook(create_if_not_exists=True)
+    hook.register_module(model)
     
-    '''
-    TODO: Create your loss and optimizer
-    '''
-    loss_criterion = None
-    optimizer = None
+    train_loader, test_loader = create_data_loaders(args.data, args.batch_size)
     
-    '''
-    TODO: Call the train function to start training your model
-    Remember that you will need to set up a way to get training data from S3
-    '''
-    model=train(model, train_loader, loss_criterion, optimizer)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.fc.parameters(), lr=args.learning_rate)
     
-    '''
-    TODO: Test the model to see its accuracy
-    '''
-    test(model, test_loader, criterion)
+    train(model, train_loader, criterion, optimizer, hook)
+    test(model, test_loader, criterion, hook)
     
-    '''
-    TODO: Save the trained model
-    '''
-    torch.save(model, path)
+    torch.save(model.state_dict(), args.model_path)
 
 if __name__=='__main__':
-    parser=argparse.ArgumentParser()
-    '''
-    TODO: Specify any training args that you might need
-    '''
-    
-    args=parser.parse_args()
-    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', type=str, default='/path/to/data')
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--learning_rate', type=float, default=0.001)
+    parser.add_argument('--epochs', type=int, default=5)
+    parser.add_argument('--model_path', type=str, default='/path/to/save/model.pth')
+    args = parser.parse_args()
     main(args)
