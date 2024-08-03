482f1417169400aa9f8d234f0a21d3188999f278
rwby00 <mashiro560@gmail.com>
2024-08-03 22:42:43 +0900

    Upload project5

--- project4/lamdafunction.py
+++ project4/lamdafunction.py
@@ -10,7 +10,7 @@ logger.setLevel(logging.DEBUG)
 print('Loading Lambda function')
 
 runtime=boto3.Session().client('sagemaker-runtime')
-endpoint_Name='BradTestEndpoint'
+endpoint_Name='pytorch-inference-2024-07-10-17-29-19-680'
 
 def lambda_handler(event, context):
 
